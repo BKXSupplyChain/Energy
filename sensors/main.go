f812ea422faf55076f719f11ec55e412794678f1
altruist <anishchenko1999@gmail.com>
2019-02-12 02:21:45 +0300

    flush

--- sensors/main.go
+++ sensors/main.go
@@ -3,53 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
-	"net"
-	"os"
-
-	"github.com/BKXSupplyChain/Energy/sensors/conf"
-	"github.com/BKXSupplyChain/Energy/utils"
+	"net/http"
 )
 
-// Can be expended in many listeners
-func startListening(idx string) {
-	connStr := conf.GetServerIPAsString(idx) + ":" + conf.GetServerPortAsString(idx)
-	fmt.Println(connStr)
-	listener, err := net.Listen("tcp", conf.GetServerIPAsString(idx)+":"+conf.GetServerPortAsString(idx))
-	utils.CheckFatal(err)
-	log.Printf("Listener %s is running\n", idx)
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			utils.CheckNotFatal(err)
-			continue
-		}
-		go handleRequest(conn)
-	}
-}
-
-// Handles incoming requests.
-func handleRequest(conn net.Conn) {
-	defer conn.Close()
-	var buf [512]byte
-	log.Printf("Serving %s\n", conn.RemoteAddr().String())
-	for {
-		len, err := conn.Read(buf[0:])
-		if err != nil {
-			log.Printf("Error: %s\n", err.Error())
-			return
-		}
-		log.Println("Number of bytes: ", len)
-		log.Println(string(buf[0:]))
-	}
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
 func main() {
-	// Set up logging
-	file, err := os.OpenFile("main.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
-	utils.CheckFatal(err)
-	defer file.Close()
-	log.SetOutput(file)
-	// Set up config
-	conf.LoadConfig("config.json")
-	startListening("1")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":1000", nil))
 }
