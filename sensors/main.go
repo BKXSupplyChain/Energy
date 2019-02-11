543344343aaf7506e6141070cd4e3b0bcfe5f6a8
altruist <anishchenko1999@gmail.com>
2019-02-12 02:27:25 +0300

    flush

--- sensors/main.go
+++ sensors/main.go
@@ -4,13 +4,26 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()       // parse arguments, you have to call this by yourself
+	fmt.Println(r.Form) // print form information in server side
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello astaxie!") // send data to client side
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":1000", nil))
+	http.HandleFunc("/", sayhelloName)       // set router
+	err := http.ListenAndServe(":1000", nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
