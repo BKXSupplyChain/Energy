3022884b41bfa8a0f9f89eafeaeaeed22f451dfa
altruist <anishchenko1999@gmail.com>
2019-02-13 13:57:52 +0300

    Json parsing

--- sensors/main.go
+++ sensors/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/BKXSupplyChain/Energy/utils"
 )
+
+type sensorPacket struct {
+	UserToken     string
+	NeighborToken string
+	Value         int64
+}
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	reqBody, err := ioutil.ReadAll(r.Body)
@@ -13,10 +23,21 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 	fmt.Printf("%s\n", reqBody)
+	var pck sensorPacket
+	err = json.Unmarshal(reqBody, &pck)
+	if err != nil {
+		utils.CheckNotFatal(err)
+		return
+	}
+	fmt.Println(pck.UserToken)
+	fmt.Println(pck.NeighborToken)
+	fmt.Println(pck.Value)
 	fmt.Fprintf(w, "OK") // send data to client side
 }
 
 func main() {
+	t := time.Now().Unix()
+	fmt.Println(t)
 	http.HandleFunc("/", sayhelloName)       // set router
 	err := http.ListenAndServe(":1000", nil) // set listen port
 	if err != nil {
