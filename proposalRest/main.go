58b97a35deb16d7b40c2f7e9e9062d7a50a54617
andrey gusev <44449418+angusev@users.noreply.github.com>
2019-03-15 22:01:00 +0300

    added SendProposal prototype

--- proposalRest/main.go
+++ proposalRest/main.go
@@ -5,17 +5,24 @@ import (
     "github.com/gorilla/mux"
     "log"
     "net/http"
+    "time"
 
     "./types"
+    "bytes"
 )
 
 var proposals []types.Proposal
+
+
+//---------------------------------
+//     REST METHODS
+//---------------------------------
 
 func CreateProposal(w http.ResponseWriter, r *http.Request) {
     params := mux.Vars(r)
     var prop types.Proposal
     _ = json.NewDecoder(r.Body).Decode(&prop)
-    prop.IP = params["IP"]
+    prop.ID = params["ID"]
     proposals = append(proposals, prop)
     json.NewEncoder(w).Encode(proposals)
 }
@@ -24,7 +31,7 @@ func DeleteProposal(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
     params := mux.Vars(r)
     for index, item := range proposals {
-        if item.IP == params["IP"] {
+        if item.ID == params["ID"] {
             proposals = append(proposals[:index], proposals[index+1:]...)
             break
         }
@@ -40,7 +47,7 @@ func GetAll(w http.ResponseWriter, r *http.Request) {
 func GetOne(w http.ResponseWriter, r *http.Request) {
     params := mux.Vars(r)
     for _, item := range proposals {
-        if item.IP == params["IP"] {
+        if item.ID == params["ID"] {
             json.NewEncoder(w).Encode(item)
             return
         }
@@ -52,11 +59,11 @@ func UpdateProposal(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
     params := mux.Vars(r)
     for index, item := range proposals{
-        if item.IP == params["IP"] {
+        if item.ID == params["ID"] {
             proposals = append(proposals[:index], proposals[index+1:]...)
             var proposal types.Proposal
             _ = json.NewDecoder(r.Body).Decode(&proposal)
-            proposal.IP = params["IP"]
+            proposal.ID = params["ID"]
             proposals = append(proposals, proposal) 
             json.NewEncoder(w).Encode(proposal)
             return
@@ -65,14 +72,42 @@ func UpdateProposal(w http.ResponseWriter, r *http.Request) {
     json.NewEncoder(w).Encode(proposals)
 }
 
-func main() {
+//--------------------------------------
+
+func SendProposal(P types.Proposal) {
+    proposals = append(proposals, P)
+    //json.NewEncoder(w).Encode(proposals)
+}
+
+func Init() *mux.Router {
     router := mux.NewRouter()
-    proposals = append(proposals, types.Proposal{IP: "1.0.0.0", Price: 1, TotalCost: 200000, DeltaPrice: 10, Salt: 42, TTL: 5})
     router.HandleFunc("/proposals", GetAll).Methods("GET")
-    router.HandleFunc("/proposals/{IP}", GetOne).Methods("GET")
-    router.HandleFunc("/proposals/{IP}", CreateProposal).Methods("POST")
-    router.HandleFunc("/proposals/{IP}", DeleteProposal).Methods("DELETE")
-    router.HandleFunc("/proposals/{IP}", UpdateProposal).Methods("PUT")
-    log.Fatal(http.ListenAndServe(":8000", router))
+    router.HandleFunc("/proposals/{ID}", GetOne).Methods("GET")
+    router.HandleFunc("/proposals/{ID}", CreateProposal).Methods("POST")
+    router.HandleFunc("/proposals/{ID}", DeleteProposal).Methods("DELETE")
+    router.HandleFunc("/proposals/{ID}", UpdateProposal).Methods("PUT")
+
+    srv := &http.Server{
+        Handler:      router,
+        Addr:         "localhost:8000",
+        WriteTimeout: 15 * time.Second,
+        ReadTimeout:  15 * time.Second,
+    }
+    log.Fatal(srv.ListenAndServe())
+
+    return router
+}
+
+func main() {
+    Init()
+    newprop := types.Proposal{ID: "1.0.0.3",
+                                                 Supplier : "A",
+                                                 Consumer : "B", 
+                                                 Price: 2, 
+                                                 TotalCost : 200001,
+                                                 DeltaPrice : 20, 
+                                                 Salt: 2, 
+                                                 TTL : 7}
+    SendProposal(newprop)
 }
 
--- proposalRest/types/types.go
+++ proposalRest/types/types.go
@@ -1,7 +1,9 @@
 package types
 
 type Proposal struct {
-    IP          string `json:"IP,omitempty"`
+    ID          string `json:"ID,omitemty"`
+    Supplier    string `json:"Supplier,omitempty"`
+    Consumer    string `json:"Consumer,omitempty"`
     Price       int32  `json:"Price,omitempty"`
     TotalCost   int32  `json:"TotalCost,omitempty"`
     DeltaPrice  int32  `json:"DeltaPrice,omitempty"`
