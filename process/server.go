c75e99fbe59a09d58a8678155f23243bdf89961d
elvarid <46623718+elvarid@users.noreply.github.com>
2019-03-22 13:28:22 +0300

    Update server.go

--- process/server.go
+++ process/server.go
@@ -10,11 +10,33 @@ import (
     "net/rpc"
 )
 
+func AddInDatabase(proposal types.Proposal)  {
+    db.Add(proposal, proposal.ID) //TODO ask about ID
+}
+
+func updateSocket(proposal types.Proposal, userPublicKey /*TODO type */, NeighbourPublicKey /*TODO type*/) {
+    socketID := string(sha256.Sum256([]byte(userPublicKey+NeighbourPublicKey))[:12])
+    var socket = types.SocketInfo
+    db.Get(&socket, socketID)
+    socket.proposals = append(socket.proposals, /* TODO what to add */
+    db.Upsert(&socket, socketID) //TODO maybe implement Update
+}
+
+func (t *types.Proposal) OnProposalReceived(proposal types.Proposal) {
+    putInDb(proposal)
+    updateSocket(proposal, /*TODO key*/, /*TODO key*/)
+}
+
 func main() {
-    cp := new(types.CertificatedPair) //check here 
+    cp := new(types.Certificate) 
     err := rpc.register(cp)
     if (err != nil) {
-         log.Fatalf("Error of format of CertificatedPair: %s", err)
+         log.Fatalf("Error of format of Certificate: %s", err)
+    }
+    proposal := new(types.Proposal)
+    err := rpc.register(proposal)
+    if (err != nil) {
+         log.Fatalf("Error of format of Proposal: %s", err)
     }
     rpc.HandleHTTP()
     listener,err = net.Listen("tcp", ":30")
