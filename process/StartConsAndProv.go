967d1802fe33ac7901c38bd5cbc48aa94624dde8
nakap21 <31996406+nakap21@users.noreply.github.com>
2019-03-27 01:09:14 +0300

    Rename server.go to StartConsAndProv.go

---
+++ process/StartConsAndProv.go
@@ -0,0 +1,16 @@
+func server(userID string) {
+	var user types.UserData
+	db.Get(&user, userID)
+
+	for ; true; {
+		for i := 0; i < len(user.Sockets); i++ {
+			consumer(user.Sockets[i], userID)
+			provider(user.Sockets[i], userID)
+		}
+		time.Sleep(time.Minute)
+		time.Sleep(time.Minute)
+		time.Sleep(time.Minute)
+		time.Sleep(time.Minute)
+
+	}
+}
