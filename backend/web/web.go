58d21a9fa9c57a390364ff1c12d40d2c18993c51
Artemiyfly <Artemiy.fly27@gmail.com>
2019-03-27 08:59:24 +0300

    Fix db.get

--- backend/web/web.go
+++ backend/web/web.go
@@ -2,19 +2,18 @@ package web
 
 import (
 	"crypto/sha256"
-	"encoding/binary"
 	"errors"
 	"fmt"
 	"github.com/BKXSupplyChain/Energy/backend/conf"
 	"github.com/BKXSupplyChain/Energy/db"
 	"github.com/BKXSupplyChain/Energy/types"
-	"hash/crc64"
 	"io"
 	"io/ioutil"
-	"log"
 	"math"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -24,20 +23,14 @@ func serveFile(path string) func(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func UsernameToId(name string) string {
-	id := make([]byte, 8)
-	binary.LittleEndian.PutUint64(id, crc64.Checksum([]byte(name), crc64.MakeTable(crc64.ECMA)))
-	return string(id)
-}
-
 func getUser(r *http.Request) (types.UserData, error) {
-	name, err := r.Cookie("username")
-	password, err := r.Cookie("password")
-	if err != nil {
-		return types.UserData{}, errors.New("Auth error")
+	name, errU := r.Cookie("username")
+	password, errP := r.Cookie("password")
+	if errU != nil || errP != nil {
+		return types.UserData{}, errors.New("Auth required")
 	}
-	log.Println("DATA GOT ", name, password)
-	id := UsernameToId(name.Value)
+
+	id := name.Value
 	var user types.UserData
 	if db.Get(&user, string(id)) != nil {
 		return types.UserData{}, errors.New("No such user")
@@ -51,14 +44,14 @@ func getUser(r *http.Request) (types.UserData, error) {
 func loginUser(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	http.SetCookie(w, &http.Cookie{
-		Name:    "username",
-		Value:   r.Form.Get("username"),
-		Expires: time.Now().AddDate(0, 0, 1),
+		Name:  "username",
+		Value: r.Form.Get("username"),
+		Path:  "/",
 	})
 	http.SetCookie(w, &http.Cookie{
-		Name:    "password",
-		Value:   r.Form.Get("password"),
-		Expires: time.Now().AddDate(0, 0, 1),
+		Name:  "password",
+		Value: r.Form.Get("password"),
+		Path:  "/",
 	})
 	http.Redirect(w, r, "/main", 307)
 }
@@ -75,8 +68,7 @@ func formatFloat(a float64) string {
 func mainData(w http.ResponseWriter, r *http.Request) {
 	user, err := getUser(r)
 	if err != nil {
-		w.Header().Add("err", "Auth error")
-		http.Redirect(w, r, "/", 307)
+		http.Redirect(w, r, "/?err=Auth error", 307)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
@@ -85,7 +77,7 @@ func mainData(w http.ResponseWriter, r *http.Request) {
 		var soc types.SocketInfo
 		db.Get(&soc, socketID)
 		w.Write([]byte(fmt.Sprintf("[\"%s\"", soc.Alias)))
-		w.Write([]byte(fmt.Sprintf(", \"%sJ/s\"", formatFloat(float64(db.TokenGetPower(UsernameToId(user.Username), time.Now().Unix()-10))))))
+		w.Write([]byte(fmt.Sprintf(", \"%sJ/s\"", formatFloat(float64(db.TokenGetPower(user.Username, time.Now().Unix()-10))))))
 		if soc.ActiveProposal != "" {
 			var prop types.Proposal
 			db.Get(&prop, soc.ActiveProposal)
@@ -101,8 +93,7 @@ func mainData(w http.ResponseWriter, r *http.Request) {
 func mainPage(w http.ResponseWriter, r *http.Request) {
 	_, err := getUser(r)
 	if err != nil {
-		w.Header().Add("err", "Auth error")
-		http.Redirect(w, r, "/", 307)
+		http.Redirect(w, r, "/?err="+err.Error(), 307)
 		return
 	}
 	pattern, _ := ioutil.ReadFile("./web/static/main.html")
@@ -113,39 +104,33 @@ func registerUser(w http.ResponseWriter, r *http.Request) {
 	var user types.UserData
 	r.ParseForm()
 	user.Username = r.Form.Get("username")
-	id := UsernameToId(user.Username)
+	id := user.Username
 	user.PasswordHash = sha256.Sum256([]byte(r.Form.Get("password")))
 	user.PrivateKey = r.Form.Get("privkey")
-	log.Println(r.Header)
 	if db.Add(&user, string(id)) != nil {
-		w.Header().Add("err", "Username is reserved")
-		http.Redirect(w, r, "/register", 307)
+		http.Redirect(w, r, "/register?err=Username is reserved", 307)
 		return
 	}
-	http.SetCookie(w, &http.Cookie{
-		Name:    "username",
-		Value:   user.Username,
-		Expires: time.Now().AddDate(0, 0, 1),
-	})
-	http.SetCookie(w, &http.Cookie{
-		Name:    "password",
-		Value:   r.Form.Get("password"),
-		Expires: time.Now().AddDate(0, 0, 1),
-	})
-	http.Redirect(w, r, "/main", 307)
+	loginUser(w, r)
 }
 
 func registerPage(w http.ResponseWriter, r *http.Request) {
-	if r.Header.Get("err") != "" {
-		w.Write([]byte("<div class=\"err\">" + r.Header.Get("err") + "</div>"))
+	if strings.HasPrefix(r.URL.RawQuery, "err=") {
+		text, err := url.QueryUnescape(r.URL.RawQuery[4:])
+		if err == nil {
+			w.Write([]byte("<div class=\"err\">" + text + "</div>"))
+		}
 	}
 	file, _ := os.Open("./web/static/register.html")
 	io.Copy(w, file)
 }
 
 func loginPage(w http.ResponseWriter, r *http.Request) {
-	if r.Header.Get("err") != "" {
-		w.Write([]byte("<div class=\"err\">" + r.Header.Get("err") + "</div>"))
+	if strings.HasPrefix(r.URL.RawQuery, "err=") {
+		text, err := url.QueryUnescape(r.URL.RawQuery[4:])
+		if err == nil {
+			w.Write([]byte("<div class=\"err\">" + text + "</div>"))
+		}
 	}
 	file, _ := os.Open("./web/static/login.html")
 	io.Copy(w, file)
